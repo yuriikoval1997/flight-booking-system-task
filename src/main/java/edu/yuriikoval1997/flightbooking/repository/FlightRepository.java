eecf4ad87bcb8508f56131502308992a8ca85c14
nomadness <vitalii.skolozdra@gmail.com>
2020-01-21 13:30:02 +0200

    Add repositories

---
+++ src/main/java/edu/yuriikoval1997/flightbooking/repository/FlightRepository.java
@@ -0,0 +1,6 @@
+package edu.yuriikoval1997.flightbooking.repository;
+
+import edu.yuriikoval1997.flightbooking.entities.Flight;
+
+public interface FlightRepository extends GenericRepository<Flight> {
+}
