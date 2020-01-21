f2a39dcca7f9e4086aac3f1c8684ba6e443dc81f
nomadness <vitalii.skolozdra@gmail.com>
2020-01-21 10:10:05 +0200

    Add private constructor

--- src/main/java/edu/yuriikoval1997/flightbooking/entities/SeatPreference.java
+++ src/main/java/edu/yuriikoval1997/flightbooking/entities/SeatPreference.java
@@ -4,4 +4,7 @@ public class SeatPreference {
     public static final int NONE = 0;
     public static final int WINDOW = 1;
     public static final int AISLE = 2;
+
+    private SeatPreference() {
+    }
 }
